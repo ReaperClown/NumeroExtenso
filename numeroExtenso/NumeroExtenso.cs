74cb47f607ae157434e48630496c91253e70f59c
Willian Mesquita <the.willian.mesquita@gmail.com>
2021-10-20 10:16:57 -0300

    Added ifelse statement to switch case to reduce the amount of code

--- numeroExtenso/NumeroExtenso.cs
+++ numeroExtenso/NumeroExtenso.cs
@@ -4,27 +4,26 @@ public class NumeroExtenso
 {
 	public static void Main()
 	{
-		int valor = 0;
 		string separador = " e ";
 
 		Console.Write("Digite um número: ");
-		valor = int.Parse(Console.ReadLine());
+		Globals.valor = int.Parse(Console.ReadLine());
 
-		if (valor < 20)
+		if (Globals.valor < 20)
 		{
-			Console.WriteLine(RetorneValorString(valor));
+			Console.WriteLine(RetorneValorString(Globals.valor));
 
 			Console.ReadLine();
 		}
 
-		if (valor > 19)
+		if (Globals.valor > 19)
 		{
-			int len = valor.ToString().Length;
+			int len = Globals.valor.ToString().Length;
 
 			if (len == 2)
 			{
-				int ValorPrimario = int.Parse(valor.ToString().Substring(0, 1));
-				int ValorSecundario = int.Parse(valor.ToString().Substring(1, 1));
+				int ValorPrimario = int.Parse(Globals.valor.ToString().Substring(0, 1));
+				int ValorSecundario = int.Parse(Globals.valor.ToString().Substring(1, 1));
 				ValorPrimario = ValorPrimario * 10;
 				Console.WriteLine(RetorneValorString(ValorPrimario) + (ValorSecundario > 0 ? separador + RetorneValorString(ValorSecundario) : ""));
 
@@ -32,25 +31,16 @@ public class NumeroExtenso
 			}
 			else if (len == 3)
 			{
-				int ValorPrimario = int.Parse(valor.ToString().Substring(0, 1));
-				int ValorSecundario = int.Parse(valor.ToString().Substring(1, 1));
-				int ValorTerciario = int.Parse(valor.ToString().Substring(2, 1));
+				int ValorPrimario = int.Parse(Globals.valor.ToString().Substring(0, 1));
+				int ValorSecundario = int.Parse(Globals.valor.ToString().Substring(1, 1));
+				int ValorTerciario = int.Parse(Globals.valor.ToString().Substring(2, 1));
 
 				ValorPrimario = ValorPrimario * 100;
 				ValorSecundario = ValorSecundario * 10;
 
-				if(valor > 100 && valor <= 199)
-                {
-					Console.WriteLine("\n" + RetorneValorString2(ValorPrimario)
+				Console.WriteLine("\n" + RetorneValorString(ValorPrimario)
 								  + (ValorSecundario > 0 ? separador + RetorneValorString(ValorSecundario) : "")
 								  + (ValorTerciario > 0 ? separador + RetorneValorString(ValorTerciario) : ""));
-				}
-                else
-                {
-					Console.WriteLine("\n" + RetorneValorString(ValorPrimario)
-								  + (ValorSecundario > 0 ? separador + RetorneValorString(ValorSecundario) : "")
-								  + (ValorTerciario > 0 ? separador + RetorneValorString(ValorTerciario) : ""));
-				}
 
 				Console.ReadLine();
 			}
@@ -91,7 +81,10 @@ public class NumeroExtenso
 			case 80: return "Oitenta";
 			case 90: return "Noventa";
 
-			case 100: return "Cem";
+			case 100: if(Globals.valor > 100 && Globals.valor <= 199)
+					  return "Cento";
+					  else
+					  return "Cem";
 			case 200: return "Duzentos";
 			case 300: return "Trezentos";
 			case 400: return "Quatrocentos";
@@ -105,52 +98,9 @@ public class NumeroExtenso
 		}
 	}
 
-	public static string RetorneValorString2(int identificador)
-	{
-		switch (identificador)
-		{
-			case 1: return "Um";
-			case 2: return "Dois";
-			case 3: return "Tres";
-			case 4: return "Quatro";
-			case 5: return "Cinco";
-			case 6: return "Seis";
-			case 7: return "Sete";
-			case 8: return "Oito";
-			case 9: return "Nove";
-
-			case 10: return "Dez";
-			case 11: return "Onze";
-			case 12: return "Doze";
-			case 13: return "Treze";
-			case 14: return "Quatorze";
-			case 15: return "Quinze";
-			case 16: return "Dezesseis";
-			case 17: return "Dezessete";
-			case 18: return "Dezoito";
-			case 19: return "Dezenove";
-
-			case 20: return "Vinte";
-			case 30: return "Trinta";
-			case 40: return "Quarenta";
-			case 50: return "Cinquenta";
-			case 60: return "Sessenta";
-			case 70: return "Setenta";
-			case 80: return "Oitenta";
-			case 90: return "Noventa";
-
-			case 100: return "Cento";
-			case 200: return "Duzentos";
-			case 300: return "Trezentos";
-			case 400: return "Quatrocentos";
-			case 500: return "Quinhentos";
-			case 600: return "Seicentos";
-			case 700: return "Setecentos";
-			case 800: return "Oitocentos";
-			case 900: return "Novecentos";
-
-			default: return "Valor inválido";
-		}
-	}
+	public static class Globals
+    {
+        public static int valor = 0;
+    }
 }
 
